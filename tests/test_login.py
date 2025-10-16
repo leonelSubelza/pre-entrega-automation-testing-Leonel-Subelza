344ef09895136979c81e1d7dee8ad652c684acc1
Leonel Subelza <subelza150@gmail.com>
2025-10-16 16:35:56 -0300

    Se agregan las carpetas base y algunos archivos

---
+++ tests/test_login.py
@@ -0,0 +1,27 @@
+from selenium import webdriver          #Importamos la librería que permite controlar el navegador
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+driver = webdriver.Chrome()
+
+def test_valid_login():
+  try:
+    # driver.implicitly_wait(5)
+    driver.get('https://www.saucedemo.com')
+    
+    #Automatización de Login:
+    driver.find_element(By.ID, "user-name").send_keys("standard_user")
+    driver.find_element(By.ID, "password").send_keys("secret_sauce")
+    driver.find_element(By.ID, "login-button").click()
+    
+    # Espera explicita a que la nueva página cargue
+    wait = WebDriverWait(driver, 10)
+    wait.until(EC.visibility_of_element_located((By.CLASS_NAME, 'inventory_item')))
+    
+    
+    #Navegación y Verificación del Catálogo: (Clases 6 a 8)
+    assert '/inventory.html' in driver.current_url, f"URL inesperada: {driver.current_url}"
+    assert driver.title == 'Swag Labs'
+  finally:
+    driver.quit()
---
+++ utils/saucedemo.py
@@ -0,0 +1,26 @@
+from selenium import webdriver          #Importamos la librería que permite controlar el navegador
+from selenium.webdriver.common.by import By
+import pytest
+import time                             #Para hacer pausas visibles (solo demo)
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException
+
+# driver = webdriver.Chrome()
+
+def login(driver,username,password):
+  driver.get('https://www.saucedemo.com')
+      
+  #Automatización de Login:
+  driver.find_element(By.ID, "user-name").send_keys(username)
+  driver.find_element(By.ID, "password").send_keys(password)
+  driver.find_element(By.ID, "login-button").click()
+  
+  # Espera a que la nueva página cargue
+  wait = WebDriverWait(driver, 10)
+  wait.until(EC.visibility_of_element_located((By.CLASS_NAME, 'inventory_item')))
+  
+  
+  #Navegación y Verificación del Catálogo: (Clases 6 a 8)
+  assert '/inventory.html' in driver.current_url, f"URL inesperada: {driver.current_url}"
+  assert driver.title == 'Swag Labs'
